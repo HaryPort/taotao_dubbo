57694c8165b95b2b0fa4f26f396de3ee594d0d74
Administrator <Administrator@AFOIG-611211054>
2017-12-10 17:03:54 +0800

    finish sso functionality

--- taotao-sso-web/src/main/java/com/taotao/sso/controller/UserController.java
+++ taotao-sso-web/src/main/java/com/taotao/sso/controller/UserController.java
@@ -3,8 +3,11 @@ package com.taotao.sso.controller;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.json.MappingJacksonValue;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +16,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.taotao.common.pojo.TaotaoResult;
 import com.taotao.common.utils.CookieUtils;
+import com.taotao.common.utils.JsonUtils;
 import com.taotao.pojo.TbUser;
 import com.taotao.sso.service.UserService;
 
@@ -49,15 +53,42 @@ public class UserController {
 	@ResponseBody
 	public TaotaoResult login(String username, String password, HttpServletRequest request, HttpServletResponse response) {
 		TaotaoResult result = userService.login(username, password);
-		//把token写入cookie
-		CookieUtils.setCookie(request, response, TOKEN_KEY, result.getData().toString());
+		//登陆成功后, 写cookie
+		if(result.getStatus() == 200) {
+			//把token写入cookie
+			CookieUtils.setCookie(request, response, TOKEN_KEY, result.getData().toString());
+		}
 		return result;
 	}
 	
-	@RequestMapping("/token/{token}")
+	/*@RequestMapping(value="/user/token/{token}", method=RequestMethod.GET, produces=MediaType.APPLICATION_JSON_UTF8_VALUE)
 	@ResponseBody
-	public TaotaoResult getUserByToken(@PathVariable String token) {
-		return userService.getUserByToken(token);
+	public String getUserByToken(@PathVariable String token, String callback) {
+		TaotaoResult result = userService.getUserByToken(token);
+		//判断是否为jsonp请求
+		if(StringUtils.isNotBlank(callback)) {
+			return callback + "(" + JsonUtils.objectToJson(result) + ");";
+		}
+		return JsonUtils.objectToJson(result);
+	}*/
+	
+	/**
+	 * jsonp 第二种处理方式, 仅限于spring4.1 以上版本使用
+	 * @param token
+	 * @param callback
+	 * @return
+	 */
+	@RequestMapping(value="/user/token/{token}", method=RequestMethod.GET)
+	@ResponseBody
+	public Object getUserByToken(@PathVariable String token, String callback) {
+		TaotaoResult result = userService.getUserByToken(token);
+		//判断是否为jsonp请求
+		if(StringUtils.isNotBlank(callback)) {
+			MappingJacksonValue mappingJacksonValue = new MappingJacksonValue(result);
+			mappingJacksonValue.setJsonpFunction(callback);
+			return mappingJacksonValue;
+		}
+		return result;
 	}
 	
 	@RequestMapping("/user/logout/{token}")
