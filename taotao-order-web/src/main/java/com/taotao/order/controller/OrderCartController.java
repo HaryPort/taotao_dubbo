a02eca7ef840c7315bc9cb6dc443d49d3451c58d
Administrator <Administrator@AFOIG-611211054>
2017-12-24 21:18:33 +0800

    Finish all the order

--- taotao-order-web/src/main/java/com/taotao/order/controller/OrderCartController.java
+++ taotao-order-web/src/main/java/com/taotao/order/controller/OrderCartController.java
@@ -6,12 +6,19 @@ import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 
 import org.apache.commons.lang3.StringUtils;
+import org.joda.time.DateTime;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.taotao.common.pojo.TaotaoResult;
 import com.taotao.common.utils.CookieUtils;
 import com.taotao.common.utils.JsonUtils;
+import com.taotao.order.pojo.OrderInfo;
+import com.taotao.order.service.OrderService;
 import com.taotao.pojo.TbItem;
 import com.taotao.pojo.TbUser;
 
@@ -25,6 +32,9 @@ public class OrderCartController {
 	
 	@Value("${TT_CART}")
 	private String TT_CART;
+	
+	@Autowired
+	private OrderService orderService;
 	
 	/*
 	 *  展示订单确认页面
@@ -55,6 +65,25 @@ public class OrderCartController {
 		}
 		
 		return new ArrayList<TbItem>();
+	}
+	
+	/**
+	 * 生成订单处理
+	 */
+	@RequestMapping(value="/order/create", method=RequestMethod.POST)
+	public String createOrder(OrderInfo orderInfo, Model model) {
+		//生成订单
+		TaotaoResult result = orderService.createOrder(orderInfo);
+		//返回逻辑视图
+		model.addAttribute("orderId", result.getData().toString());
+		model.addAttribute("payment", orderInfo.getPayment());
+		
+		//预计送达时间, 预计三天送到
+		DateTime dateTime = new DateTime();
+		dateTime = dateTime.plusDays(3);
+		model.addAttribute("date", dateTime.toString("yyyy-MM-dd"));
+		
+		return "success";
 	}
 	
 
